f79507e59dd51171cbed2732c9057ba5b96bdf25
Akshaya <akshayachigullapally1@gmail.com>
2025-08-03 16:03:41 +0530

    added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 import App from './App.jsx'
-import 'react-toastify/dist/ReactToastify.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
